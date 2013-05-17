05fddf0ba8b6c63b1bf7e1179a78ea6ac5d8d8a2
Andreas Weber <andreas.weber@hs-offenburg.de>
2013-05-17 17:02:46 +0200

    remove SUB(stitute) and windows line ending from some original hershey fonts

--- hershey_fonts/fix_linebreak.sh
+++ hershey_fonts/fix_linebreak.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "removing unwanted linebreak in $1..."
-awk --re-interval 'BEGIN{FIELDWIDTHS = "5 3 1 1 5000"} $1~/[ 0-9]{5}/{printf("\n")} {printf("%s",$0)}' $1 > ../fixed/$1
+sed 's/\x1A//g' $1 | tr -d '\r' | awk --re-interval 'BEGIN{FIELDWIDTHS = "5 3 1 1 200"} $1~/[ 0-9]{5}/{if(NR>1) printf("\n")} {printf("%s",$0)}' > ../fixed/$1
