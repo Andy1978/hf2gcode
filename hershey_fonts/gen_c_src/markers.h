627cc000f9953ba660e9f5acb635d8013f0e8c3c
Andreas Weber <andy.weber.aw@gmail.com>
2013-05-25 12:01:47 +0200

    added generated font header so that GNU Octave is optional

---
+++ hershey_fonts/gen_c_src/markers.h
@@ -0,0 +1,106 @@
+/* This file was generated with gen_C_code.m from fixed/markers.jhf */
+
+/* number of glyphs in font */
+unsigned char markers_cnt = 97;
+
+/* Format: margin left, margin right, X, Y ... \0
+< R> is pen-up, see original hershey font */
+const char markers [] PROGMEM="JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "KYQKNLLNKQKSLVNXQYSYVXXVYSYQXNVLSKQK\0"\
+          "LXLLLXXXXLLL\0"\
+          "KYRJKVYVRJ\0"\
+          "LXRHLRR\\XRRH\0"\
+          "JZRIPOJOOSMYRUWYUSZOTORI\0"\
+          "LXPLPPLPLTPTPXTXTTXTXPTPTLPL\0"\
+          "KYRKRY RKRYR\0"\
+          "MWMMWW RWMMW\0"\
+          "MWRLRX RMOWU RWOMU\0"\
+          "NVQNOONQNSOUQVSVUUVSVQUOSNQN ROQOS RPPPT RQOQU RRORU RSOSU RTPTT RUQUS\0"\
+          "NVNNNVVVVNNN ROOOU RPOPU RQOQU RRORU RSOSU RTOTU RUOUU\0"\
+          "MWRLMUWURL RROOT RROUT RRRQT RRRST\0"\
+          "LULRUWUMLR RORTU RORTO RRRTS RRRTQ\0"\
+          "MWRXWOMORX RRUUP RRUOP RRRSP RRRQP\0"\
+          "OXXROMOWXR RURPO RURPU RRRPQ RRRPS\0"\
+          "D`DR`R RDRRb R`RRb\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "KYQKNLLNKQKSLVNXQYSYVXXVYSYQXNVLSKQK\0"\
+          "LXLLLXXXXLLL\0"\
+          "KYRJKVYVRJ\0"\
+          "LXRHLRR\\XRRH\0"\
+          "JZRIPOJOOSMYRUWYUSZOTORI\0"\
+          "LXPLPPLPLTPTPXTXTTXTXPTPTLPL\0"\
+          "KYRKRY RKRYR\0"\
+          "MWMMWW RWMMW\0"\
+          "MWRLRX RMOWU RWOMU\0"\
+          "NVQNOONQNSOUQVSVUUVSVQUOSNQN ROQOS RPPPT RQOQU RRORU RSOSU RTPTT RUQUS\0"\
+          "NVNNNVVVVNNN ROOOU RPOPU RQOQU RRORU RSOSU RTOTU RUOUU\0"\
+          "MWRLMUWURL RROOT RROUT RRRQT RRRST\0"\
+          "LULRUWUMLR RORTU RORTO RRRTS RRRTQ\0"\
+          "MWRXWOMORX RRUUP RRUOP RRRSP RRRQP\0"\
+          "OXXROMOWXR RURPO RURPU RRRPQ RRRPS\0"\
+          "D`DR`R RDRRb R`RRb\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+          "JZ\0"\
+;
+/* End of file gen_c_src/markers.h */
---
+++ hershey_fonts/gen_c_src/mathupp.h
@@ -0,0 +1,105 @@
+/* This file was generated with gen_C_code.m from fixed/mathupp.jhf */
+
+/* number of glyphs in font */
+unsigned char mathupp_cnt = 96;
+
+/* Format: margin left, margin right, X, Y ... \0
+< R> is pen-up, see original hershey font */
+const char mathupp [] PROGMEM="JZ\0"\
+          "F^RJR[ RJRZR RJ[Z[\0"\
+          "F^RJR[ RJJZJ RJRZR\0"\
+          "G]KKYY RYKKY\0"\
+          "PURQRRSRSQRQ\0"\
+          "F^[FI[ RNFPHPJOLMMKMIKIIJGLFNFPGSHVHYG[F RWTUUTWTYV[X[ZZ[X[VYTWT\0"\
+          "F^ZFJMZT RJVZV RJ[Z[\0"\
+          "F^JFZMJT RJVZV RJ[Z[\0"\
+          "KYVBTDRGPKOPOTPYR]T`Vb RTDRHQKPPPTQYR\\T`\0"\
+          "KYNBPDRGTKUPUTTYR]P`Nb RPDRHSKTPTTSYR\\P`\0"\
+          "JZRLRX RMOWU RWOMU\0"\
+          "E_RIR[ RIR[R\0"\
+          "MWSZR[QZRYSZS\\R^Q_\0"\
+          "E_IR[R\0"\
+          "MWRYQZR[SZRY\0"\
+          "G][BIb\0"\
+          "H\\QFNGLJKOKRLWNZQ[S[VZXWYRYOXJVGSFQF\0"\
+          "H\\NJPISFS[\0"\
+          "H\\LKLJMHNGPFTFVGWHXJXLWNUQK[Y[\0"\
+          "H\\MFXFRNUNWOXPYSYUXXVZS[P[MZLYKW\0"\
+          "H\\UFKTZT RUFU[\0"\
+          "H\\WFMFLOMNPMSMVNXPYSYUXXVZS[P[MZLYKW\0"\
+          "H\\XIWGTFRFOGMJLOLTMXOZR[S[VZXXYUYTXQVOSNRNOOMQLT\0"\
+          "H\\YFO[ RKFYF\0"\
+          "H\\PFMGLILKMMONSOVPXRYTYWXYWZT[P[MZLYKWKTLRNPQOUNWMXKXIWGTFPF\0"\
+          "H\\XMWPURRSQSNRLPKMKLLINGQFRFUGWIXMXRWWUZR[P[MZLX\0"\
+          "AcHBHb RIBIb R[B[b R\\B\\b RDB`B RDbMb RWb`b\0"\
+          "BaGBQPFb RFBPP REBPQ REB\\B^I[B RGa\\a RFb\\b^[[b\0"\
+          "F^ZIJRZ[\0"\
+          "E_IO[O RIU[U\0"\
+          "F^JIZRJ[\0"\
+          "E_YIK[ RIO[O RIU[U\0"\
+          "E_IM[M RIR[R RIW[W\0"\
+          "I[RFJ[ RRFZ[ RMTWT\0"\
+          "G\\KFK[ RKFTFWGXHYJYLXNWOTP RKPTPWQXRYTYWXYWZT[K[\0"\
+          "H]ZKYIWGUFQFOGMILKKNKSLVMXOZQ[U[WZYXZV\0"\
+          "G\\KFK[ RKFRFUGWIXKYNYSXVWXUZR[K[\0"\
+          "H[LFL[ RLFYF RLPTP RL[Y[\0"\
+          "HZLFL[ RLFYF RLPTP\0"\
+          "H]ZKYIWGUFQFOGMILKKNKSLVMXOZQ[U[WZYXZVZS RUSZS\0"\
+          "G]KFK[ RYFY[ RKPYP\0"\
+          "NVRFR[\0"\
+          "JZVFVVUYTZR[P[NZMYLVLT\0"\
+          "G\\KFK[ RYFKT RPOY[\0"\
+          "HYLFL[ RL[X[\0"\
+          "F^JFJ[ RJFR[ RZFR[ RZFZ[\0"\
+          "G]KFK[ RKFY[ RYFY[\0"\
+          "G]PFNGLIKKJNJSKVLXNZP[T[VZXXYVZSZNYKXIVGTFPF\0"\
+          "G\\KFK[ RKFTFWGXHYJYMXOWPTQKQ\0"\
+          "G]PFNGLIKKJNJSKVLXNZP[T[VZXXYVZSZNYKXIVGTFPF RSWY]\0"\
+          "G\\KFK[ RKFTFWGXHYJYLXNWOTPKP RRPY[\0"\
+          "H\\YIWGTFPFMGKIKKLMMNOOUQWRXSYUYXWZT[P[MZKX\0"\
+          "JZRFR[ RKFYF\0"\
+          "G]KFKULXNZQ[S[VZXXYUYF\0"\
+          "I[JFR[ RZFR[\0"\
+          "F^HFM[ RRFM[ RRFW[ R\\FW[\0"\
+          "H\\KFY[ RYFK[\0"\
+          "I[JFRPR[ RZFRP\0"\
+          "H\\YFK[ RKFYF RK[Y[\0"\
+          "KYOBOb RPBPb ROBVB RObVb\0"\
+          "KYKFY^\0"\
+          "KYTBTb RUBUb RNBUB RNbUb\0"\
+          "F_[WYWWVUTRPQOONMNKOJQJSKUMVOVQURTUPWNYM[M\0"\
+          "F_\\S[UYVWVUUTTQPPONNLNJOIQISJULVNVPUQTTPUOWNYN[O\\Q\\S\0"\
+          "KYQFOGNINKOMQNSNUMVKVIUGSFQF\0"\
+          "JZRLRX RMOWU RWOMU\0"\
+          "E[HMLMRY RKMR[ R[BR[\0"\
+          "AbDMIMRY RHNR[ Rb:R[\0"\
+          "F^ZJSJOKMLKNJQJSKVMXOYSZZZ\0"\
+          "F^JJJQKULWNYQZSZVYXWYUZQZJ\0"\
+          "F^JJQJUKWLYNZQZSYVWXUYQZJZ\0"\
+          "F^JZJSKOLMNKQJSJVKXMYOZSZZ\0"\
+          "F^ZJSJOKMLKNJQJSKVMXOYSZZZ RJRVR\0"\
+          "E_XP[RXT RUMZRUW RIRZR\0"\
+          "JZPLRITL RMORJWO RRJR[\0"\
+          "E_LPIRLT ROMJROW RJR[R\0"\
+          "JZPXR[TX RMURZWU RRIRZ\0"\
+          "I\\XRWOVNTMRMONMQLTLWMYNZP[R[UZWXXUYPYKXHWGUFRFPGOHOIPIPH RRMPNNQMTMXNZ RR[TZVXWUXPXKWHUF\0"\
+          "H\\JFR[ RKFRY RZFR[ RJFZF RKGYG\0"\
+          "AbDMIMRY RHNR[ Rb:R[\0"\
+          "F^[CZD[E\\D\\C[BYBWCUETGSJRNPZO^N` RVDUFTJRVQZP]O_MaKbIbHaH`I_J`Ia\0"\
+          "Ca].\\.[/[0\\1]1^0^.],[+Y+W,U.T0S3R:QJQjPsOv R\\/\\0]0]/\\/ RR:Rj RU.T1S:SZRjQqPtOvMxKyIyGxFvFtGsHsItIuHvGv RGtGuHuHtGt\0"\
+          "I[X+U1R8P=OANFMNMVN^OcPgRlUsXy RU1S6Q<P@OFNNNVO^PdQhSnUs\0"\
+          "I[L+O1R8T=UAVFWNWVV^UcTgRlOsLy RO1Q6S<T@UFVNVVU^TdShQnOs\0"\
+          "I[M+MRMy RN+NRNy RM+X+ RMyXy\0"\
+          "I[V+VRVy RW+WRWy RL+W+ RLyWy\0"\
+          "I\\XFX[ RKFXF RPPXP RK[X[\0"\
+          "H\\KILKXWYYY[ RLLXX RKIKKLMXYY[ RPPLTKVKXLZK[ RKVMZ RLTLVMXMZK[ RSSXN RVIVLWNYNYLWKVI RVIWLYN\0"\
+          "E_RIQJRKSJRI RIR[R RRYQZR[SZRY\0"\
+          "KYOBOb RUBUb\0"\
+          "F^RBR[ RI[[[\0"\
+          "KYTBTb RUBUb RNBUB RNbUb\0"\
+          "F^[BI[[[\0"\
+          "KYTBRCQDPFPHQJRKSMSOQQ RRCQEQGRISJTLTNSPORSTTVTXSZR[Q]Q_Ra RQSSUSWRYQZP\\P^Q`RaTb\0"\
+          "E_RIQJRKSJRI RIYHZI[JZIY R[YZZ[[\\Z[Y\0"\
+          "F^IUISJPLONOPPTSVTXTZS[Q RISJQLPNPPQTTVUXUZT[Q[O\0"\
+;
+/* End of file gen_c_src/mathupp.h */
